a12fc981750f90d18b48b428214e97719b146929
Ulysse Moreau <ulysse.moreau@gmx.fr>
2025-06-22 12:11:39 +0200

    Set custom config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,4 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+require("lspconfig").pyright.setup({})
+require("lspconfig").clangd.setup({})
+
+-- read :h vim.lsp.config for changing options of lsp servers
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,11 +8,79 @@ return {
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
+    keys = {
+        { "<leader>fu", "<cmd>lua require(\"telescope.builtin\").lsp_references()<cr>", desc = "LazyGit" }
+    },
     config = function()
       require "configs.lspconfig"
     end,
   },
 
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+      },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+  {
+    "ej-shafran/compile-mode.nvim",
+    version = "^5.0.0",
+    -- you can just use the latest version:
+    branch = "latest",
+    -- or the most up-to-date updates:
+    -- branch = "nightly",
+    lazy = true,
+    cmd = {
+        "Compile",
+        "NextError",
+        "PrevError",
+        "CurrentError",
+        "FirstError",
+        "QuickfixErrors" ,
+     },
+    keys = {
+        { "<leader>cm", "<cmd>Compile<cr>", desc = "Compile" }
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- if you want to enable coloring of ANSI escape codes in
+      -- compilation output, add:
+      { "m00qek/baleia.nvim", tag = "v1.3.0" },
+    },
+    -- config = function()
+      -- ---@type CompileModeOpts
+    --  vim.g.compile_mode = {
+          -- to add ANSI escape code support, add:
+    --      baleia_setup = true,
+
+          -- to make `:Compile` replace special characters (e.g. `%`) in
+          -- the command (and behave more like `:!`), add:
+          -- bang_expansion = true,
+    --  }
+    --end
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
