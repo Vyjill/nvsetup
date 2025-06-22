de839ad346b16b9707274c01f59b15c6078d8498
Ulysse Moreau <ulysse.moreau@gmx.fr>
2025-06-22 14:55:21 +0200

    Use better undotree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -120,13 +120,11 @@ return {
     },
   },
   {
-    "mbbill/undotree",
-    keys = {
-      {
-        "<leader>u",
-        vim.cmd.UndotreeToggle,
-        desc = "UndoTree - Toggle",
-      },
+    "jiaoshijie/undotree",
+    dependencies = {"nvim-lua/plenary.nvim"},
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
     },
   },
   -- test new blink
